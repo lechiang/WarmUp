27a6b387059151c5c8c34a04b09cb6e05ec57f46
Lesley <chiang.lesley@gmail.com>
2014-02-14 04:37:38 -0800

    update4

--- testAdditional.py
+++ testAdditional.py
@@ -31,17 +31,6 @@ class TestAddUser(testLib.RestTestCase):
         respData = self.makeRequest("/users/create", method="POST", data={'user':'user2', 'password':'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'})
         self.assertResponse(respData, None, testLib.RestTestCase.ERR_BAD_PASSWORD)
 
-class TestLoginUser(testLib.RestTestCase):
-    
-    def assertResponse(self, respData, count = 1, errCode = testLib.RestTestCase.SUCCESS):
-        """
-        Check that the response data dictionary matches the expected values
-        """
-        expected = { 'errCode' : errCode }
-        if count is not None:
-            expected['count']  = count
-        self.assertDictEqual(expected, respData)
-
     def testLogin(self):
         user = self.makeRequest("/users/create", method="POST", data = {'user':'user3', 'password':'pwd3'})
         respData = self.makeRequest("/users/login", method="POST", data = {'user':'user3', 'password':'pwd3'})
@@ -51,17 +40,6 @@ class TestLoginUser(testLib.RestTestCase):
         user = self.makeRequest("/users/create", method="POST", data = {'user':'user4', 'password':'pwd4'})
         respData = self.makeRequest("/users/login", method="POST", data = {'user':'user4', 'password':'pwd5'})
         self.assertResponse(respData, None, testLib.RestTestCase.ERR_BAD_CREDENTIALS)
-
-class TestResetFixture(testLib.RestTestCase):
-    
-    def assertResponse(self, respData, count = 1, errCode = testLib.RestTestCase.SUCCESS):
-        """
-        Check that the response data dictionary matches the expected values
-        """
-        expected = { 'errCode' : errCode }
-        if count is not None:
-            expected['count']  = count
-        self.assertDictEqual(expected, respData)
 
     def testReset(self):
         user = self.makeRequest("/users/create", method="POST", data = {'user':'user5', 'password':'pwd5'})
